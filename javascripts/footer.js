ad2ac15024c208b69cd5840e9fc9dc601b663d65
Brandon Rohrer <brohrer@gmail.com>
2016-08-27 12:35:19 -0400

    Create footer.js

---
+++ javascripts/footer.js
@@ -0,0 +1,10 @@
+document.write('\
+    <!-- FOOTER  -->\
+    <div id="footer_wrap" class="outer">\
+      <footer class="inner">\
+        <section class="clearfix">\
+          <a id="project_tagline" href="blog.html"><span>Data Science and Robots Blog</span></a>\
+        </section>\
+      </footer>\
+    </div>\
+');
