c88e90e518d371db4a6ab38cda03a98ec11e54fe
Brandon Rohrer <brohrer@gmail.com>
2016-08-27 15:21:14 -0400

    Update blog_head.js

--- javascripts/blog_head.js
+++ javascripts/blog_head.js
@@ -2,10 +2,10 @@ document.write('\
   <head>\
     <meta charset='utf-8'>\
     <meta http-equiv="X-UA-Compatible" content="chrome=1">\
-    <meta name="description" content="Brandon Rohrer: ' + blog_title + '">\
+    <meta name="description" content="Brandon Rohrer:  + blog_title + ">\
     <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" media="screen">\
     <link rel="stylesheet" type="text/css" href="stylesheets/print.css" media="print">\
     <base target="_blank">\
-    <title>' + blog_title + '</title>\
+    <title> + blog_title + </title>\
   </head>\
 ');
