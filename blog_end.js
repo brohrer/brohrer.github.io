6d957a5cf3a1033360bab97897f5a32c47d72b4e
Brandon Rohrer <brohrer@gmail.com>
2016-08-27 11:35:46 -0400

    Update and rename blog_footer.js to blog_end.js

---
+++ blog_end.js
@@ -0,0 +1,44 @@
+document.write('\
+        <!-- SIGNATURE -->
+        <p>
+          <a href="http://brohrer.github.io/index.html">Brandon</a>
+          <br>
+          <a href="https://twitter.com/_brohrer_">@_brohrer_</a>
+          <br>
+          <a href="https://www.facebook.com/DataScienceAndRobots/">Facebook</a>
+          <br>
+          <a href="https://www.linkedin.com/in/brohrer">LinkedIn</a>
+        </p>
+        <p>
+        If you found this helpful, I also recommend the
+        <a href="blog.html">Data Science and Robots Blog</a>.
+        <br>
+        I work at Microsoft, but my opinions are my own.
+        </p>
+      </section>
+    </div>
+
+    <!-- FOOTER  -->
+    <div id="footer_wrap" class="outer">
+      <footer class="inner">
+        <section class="clearfix">
+          <a id="project_tagline" href="blog.html"><span>Blog</span></a>
+          &nbsp &nbsp &nbsp
+          <a id="project_tagline" href="index.html"><span>Home</span></a>
+        </section>
+      </footer>
+    </div>
+
+    <script type="text/javascript">
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+      try {
+        var pageTracker = _gat._getTracker("UA-10180621-3");
+      pageTracker._trackPageview();
+      } catch(err) {}
+    </script>
+  </body>
+</html>
+');
