7231fba446d9de15a570eca5fdb045e889c12798
Brandon Rohrer <brohrer@gmail.com>
2016-08-27 15:17:02 -0400

    Update blog_head.js

--- javascripts/blog_head.js
+++ javascripts/blog_head.js
@@ -1,1 +1,11 @@
-document.write('My favorite subject is ' + blog_title + ' not science.');
+document.write('My favorite subject is ' + blog_title + '/
+  <head>/
+    <meta charset='utf-8'>/
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">/
+    <meta name="description" content="Brandon Rohrer: Blog title.">/
+    <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" media="screen">/
+    <link rel="stylesheet" type="text/css" href="stylesheets/print.css" media="print">/
+    <base target="_blank">/
+    <title>Blog title</title>/
+  </head>/
+');
