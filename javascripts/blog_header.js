ad4dc765584c1e67ff410456dd68697721988afc
Brandon Rohrer <brohrer@gmail.com>
2016-08-27 15:27:13 -0400

    Update blog_header.js

--- javascripts/blog_header.js
+++ javascripts/blog_header.js
@@ -2,7 +2,7 @@ document.write('\
     <!-- HEADER -->\
     <div id="header_wrap" class="outer">\
         <header class="inner">\
-          <h1 id="project_title">Blog Title</h1>\
+          <h1 id="project_title">' + blog_title + '</h1>\
           <h2><a id="project_tagline" href="blog.html">\
             Data Science and Robots Blog</a></h2>\
         </header>\
