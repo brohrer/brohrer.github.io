a88e9475a886c1b8526af8945ec78740f398a8b9
Brandon Rohrer <brohrer@gmail.com>
2016-08-27 12:40:49 -0400

    Create blog_header.js

---
+++ javascripts/blog_header.js
@@ -0,0 +1,10 @@
+document.write('\
+    <!-- HEADER -->\
+    <div id="header_wrap" class="outer">\
+        <header class="inner">\
+          <h1 id="project_title">Blog Title</h1>\
+          <h2><a id="project_tagline" href="blog.html">\
+            Data Science and Robots Blog</a></h2>\
+        </header>\
+    </div>\
+');
