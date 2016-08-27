bba71af218cabaf3e52bdbe32ff867b5baf38b17
Brandon Rohrer <brohrer@gmail.com>
2016-08-27 11:39:55 -0400

    Update blog_end.js

--- blog_end.js
+++ blog_end.js
@@ -28,17 +28,4 @@ document.write('\
         </section>
       </footer>
     </div>
-
-    <script type="text/javascript">
-      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-    </script>
-    <script type="text/javascript">
-      try {
-        var pageTracker = _gat._getTracker("UA-10180621-3");
-      pageTracker._trackPageview();
-      } catch(err) {}
-    </script>
-  </body>
-</html>
 ');
