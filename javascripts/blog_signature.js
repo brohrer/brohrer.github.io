f0ac5457e0cf65267e763cf1ba2085e771bc6988
Brandon Rohrer <brohrer@gmail.com>
2016-08-31 08:43:18 -0400

    Update blog_signature.js

--- javascripts/blog_signature.js
+++ javascripts/blog_signature.js
@@ -6,14 +6,14 @@ document.write('\
           ' + publication_date + '\
         <\p>\
         <p>\
+          <a href="https://www.linkedin.com/in/brohrer">\
+            <img title="LinkedIn profile" src="images/linkedin_icon.png" alt="LinkedIn profile" style="width: 40px;" />\
+          </a>\
           <a href="https://www.facebook.com/DataScienceAndRobots/">\
             <img title="Data Science and Robots Facebook page" src="images/facebook_icon.png" alt="Facebook page" style="width: 40px;" />\
           </a>\
           <a href="https://twitter.com/_brohrer_">\
             <img title="@_brohrer_" src="images/twitter_logo.png" alt="Twitter profile" style="width: 40px;" />\
-          </a>\
-          <a href="https://www.linkedin.com/in/brohrer">\
-            <img title="LinkedIn profile" src="images/linkedin_icon.png" alt="LinkedIn profile" style="width: 40px;" />\
           </a>\
         </p>\
 ');
