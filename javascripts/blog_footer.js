80976245dcdc8f3a89329c49db6351f4fd9d17fb
Brandon Rohrer <brohrer@gmail.com>
2016-10-19 14:18:00 -0400

    Update blog_footer.js

--- javascripts/blog_footer.js
+++ javascripts/blog_footer.js
@@ -4,9 +4,11 @@ document.write('\
       <footer class="inner">\
         <section class="clearfix">\
           <a id="project_tagline" href="blog.html"><span>Data Science and Robots Blog</span></a>\
-          <a href="https://creativecommons.org/publicdomain/zero/1.0/">\
-            <img title="Creative Commons Zero license" src="images/cc0.png" alt="CC0" style="width: 50px;" />\
-          </a> except where noted.\
+          <p>\
+            <a href="https://creativecommons.org/publicdomain/zero/1.0/">\
+              <img title="Creative Commons Zero license" src="images/cc0.png" alt="CC0" style="width: 50px;" />\
+            </a> except where noted.\
+          </p>\
         </section>\
       </footer>\
     </div>\
