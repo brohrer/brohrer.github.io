f395e05ae658d3ba7eabc52fb5773f898d29e750
Brandon Rohrer <brohrer@gmail.com>
2017-10-31 23:26:58 -0400

    Create becca_header.js

---
+++ javascripts/becca_header.js
@@ -0,0 +1,10 @@
+document.write('\
+    <!-- HEADER -->\
+    <div id="header_wrap" class="outer">\
+        <header class="inner">\
+          <h1 id="project_title">' + blog_title + '</h1>\
+          <h2><a id="project_tagline" href="becca.html">\
+            Becca</a></h2>\
+        </header>\
+    </div>\
+');
