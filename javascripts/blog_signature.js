6a155d72e8b6ce15111808c152f841b558eca436
Brandon Rohrer <brohrer@gmail.com>
2019-03-10 10:10:49 -0400

    Update blog_signature.js

--- javascripts/blog_signature.js
+++ javascripts/blog_signature.js
@@ -6,11 +6,6 @@ document.write('\
           ' + publication_date + '\
         <\p>\
         <p>\
-          <!--\
-          <a href="https://www.facebook.com/DataScienceAndRobots/">\
-            <img title="Data Science and Robots Facebook page" src="images/facebook_icon.png" alt="Facebook page" style="width: 40px;" />\
-          </a>\
-          -->\
           <a href="https://twitter.com/_brohrer_">\
             <img title="@_brohrer_" src="images/twitter_logo.png" alt="Twitter profile" style="width: 40px;" />\
           </a>\
