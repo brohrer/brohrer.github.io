30a8124bc3c109bc3807c8be3dece260c63fa3f5
brohrer <brohrer@gmail.com>
2019-08-13 12:55:40 -0400

    header update

--- javascripts/blog_header.js
+++ javascripts/blog_header.js
@@ -3,8 +3,8 @@ document.write('\
     <div id="header_wrap" class="outer">\
         <header class="inner">\
           <h1 id="project_title">' + blog_title + '</h1>\
-          <h2><a id="project_tagline" href="blog.html">\
-            End-to-End Machine Learning</a></h2>\
+            <h4><a id="project_tagline" href="https://end-to-end-machine-learning.teachable.com/p/complete-course-library-full-end-to-end-machine-learning-catalog">\
+            Come visit the complete End-to-End Machine Learning course library</a></h4>\
         </header>\
     </div>\
 ');
