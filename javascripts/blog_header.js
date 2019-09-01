2307a4de4bc1ea7cbe308dcfe1a00e4ba26f9b05
brohrer <brohrer@gmail.com>
2019-09-01 09:49:29 -0400

    blog links modified

--- javascripts/blog_header.js
+++ javascripts/blog_header.js
@@ -4,8 +4,8 @@ document.write('\
         <header class="inner">\
           <h1 id="project_title">' + blog_title + '</h1>\
             <h4>\
-              <a id="project_tagline" href="https://end-to-end-machine-learning.teachable.com/p/complete-course-library-full-end-to-end-machine-learning-catalog">\
-                part of the End-to-End Machine Learning School course library\
+              <a id="project_tagline" href="blog.html">\
+                part of the End-to-End Machine Learning library\
               </a>\
             </h4>\
         </header>\
